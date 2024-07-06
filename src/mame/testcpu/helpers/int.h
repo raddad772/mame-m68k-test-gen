65211e2f99f6e0c649b91f3079a1cf4d14281b31
raddad772 <81476102+raddad772@users.noreply.github.com>
2024-07-06 09:06:44 -0400

    more progress toward m68k testing

---
+++ src/mame/testcpu/helpers/int.h
@@ -0,0 +1,102 @@
+#ifndef _JSMOOCH_INT_H
+#define _JSMOOCH_INT_H
+
+
+#define COMPILER_CLANG 3
+#define BUILD_COMPILER COMPILER_CLANG
+#define CPU_ARM64 5
+#define HOST_CPU CPU_ARM64
+
+/* If you are somehow on a big-endian platform, you must change this */
+#define ENDIAN_LITTLE
+//#define ENDIAN_BIG
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef uint64 u64;
+typedef int64 s64;
+typedef int64 i64;
+
+typedef uint32 u32;
+typedef int32 s32;
+typedef int32 i32;
+
+typedef uint16 u16;
+typedef int16 s16;
+typedef int16 i16;
+
+typedef uint8 u8;
+typedef int8 s8;
+typedef int8 i8;
+
+typedef float f32;
+typedef double f64;
+
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+
+#define SIGNe4to32(x) ((((x) >> 3) * 0xFFFFFFF0) | ((x) & 0x0F))
+#define SIGNe8to32(x) (((((x) >> 7) & 1) * 0xFFFFFF00) | ((x) & 0xFF))
+#define SIGNe8to64(x) (((((x) >> 7) & 1) * 0xFFFFFFFFFFFFFF00) | ((x) & 0xFF))
+#define SIGNe12to32(x) (((((x) >> 11) & 1) * 0xFFFFF000) | ((x) & 0xFFF))
+#define SIGNe16to32(x) (((((x) >> 15) & 1) * 0xFFFF0000) | ((x) & 0xFFFF))
+
+#if defined(__APPLE__)
+// Mac os X
+#include <libkern/OSByteOrder.h>
+#define bswap_16(x) OSSwapInt16(x)
+#define bswap_32(x) OSSwapInt32(x)
+#define bswap_64(x) OSSwapInt64(x)
+#else
+#if HAVE_BYTESWAP_H
+#include <byteswap.h>
+#else
+#define bswap_16 __builtin_bswap16
+#define bswap_32 __builtin_bswap32
+#define bswap_64 __builtin_bswap64
+#endif
+#endif
+
+#ifdef ENDIAN_LITTLE
+
+#define from_big16(x) bswap_16(x)
+#define from_big32(x) bswap_32(x)
+#define from_big64(x) bswap_64(x)
+
+#define from_little16(x) (x)
+#define from_little32(x) (x)
+#define from_little64(x) (x)
+
+#else
+
+#define from_big16(x) (x)
+#define from_big32(x) (x)
+#define from_big64(x) (x)
+
+#define from_little16(x) bswap_16(x)
+#define from_little32(x) bswap_32(x)
+#define from_little64(x) bswap_64(x)
+
+
+#endif
+
+#endif
---
+++ src/mame/testcpu/teststuff.h
@@ -0,0 +1,70 @@
+#include "helpers/multisize_memaccess.c"
+
+#define M8 1
+#define M16 2
+#define M32 4
+#define M64 8
+
+#define R8 cR[M8](ptr, 0); ptr += 1
+#define R16 cR[M16](ptr, 0); ptr += 2
+#define R32 cR[M32](ptr, 0); ptr += 4
+#define R64 cR[M64](ptr, 0); ptr += 8
+#define W8(v) cW[M8](ptr, 0); ptr += 1
+#define W16(v) cW[M16](ptr, 0); ptr += 2
+#define W32(v) cW[M32](ptr, 0); ptr += 4
+#define W64(v) cW[M64](ptr, 0); ptr += 8
+
+#define MAX_RAM_PAIRS 100
+#define MAX_TRANSACTIONS 100
+
+enum transaction_kind {
+    tk_idle_cycles,
+    tk_read,
+    tk_write,
+    tk_tas
+};
+
+struct transaction {
+    enum transaction_kind kind;
+    u32 len;
+    u32 fc;
+    u32 addr_bus;
+    u32 sz;
+    u32 data_bus;
+    u32 UDS, LDS;
+
+    u32 visited;
+
+    u32 start_cycle;
+};
+
+struct RAM_pair {
+    u32 addr;
+    u8 val;
+};
+
+struct m68k_test_transactions {
+    u32 num_transactions;
+    struct transaction items[MAX_TRANSACTIONS];
+};
+
+struct m68k_test_state {
+    u32 d[8];
+    u32 a[7];
+    u32 usp, ssp, sr, pc;
+    u32 prefetch[2];
+    u32 num_RAM;
+    struct RAM_pair RAM_pairs[MAX_RAM_PAIRS];
+};
+
+// individual test
+struct m68k_test {
+    char name[100];
+    u32 opcode;
+    struct m68k_test_state initial;
+    struct m68k_test_state final;
+    struct m68k_test_transactions transactions;
+    u32 num_cycles;
+
+    u32 current_cycle;
+};
